7547ca3c0d5f809b4f8cab6ffe211d2a27e7d547
nhatnguyen26 <nathan.nguyen2604@gmail.com>
2013-06-21 17:54:04 +0900

    second

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -7,6 +7,14 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
+		
+		int a = 2;
+		if (a>4){
+			System.out.println("test");
+		}
+		else {
+			System.out.println("hah");
+		}
 
 	}
 
